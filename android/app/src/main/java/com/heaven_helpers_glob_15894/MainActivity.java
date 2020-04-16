463865d3644ed16e2c06bcc7bc2dd5b3eac4072c
Crowdbotics <team@crowdbotics.com>
2020-04-16 01:47:20 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/heaven_helpers_glob_15894/MainActivity.java
@@ -0,0 +1,15 @@
+package com.heaven_helpers_glob_15894;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "heaven_helpers_glob_15894";
+    }
+}
